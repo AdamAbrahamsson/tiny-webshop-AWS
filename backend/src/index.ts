3b78d0f6681df724cf1a97cf48092b9f21c1ea0c
haja <haja@student.chalmers.se>
2025-09-28 21:54:32 +0200

    modify the listener for the health check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -35,6 +35,9 @@ app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+//app.listen(PORT, () => {
+  //console.log(`Server running on http://localhost:${PORT}`);
+//});
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
